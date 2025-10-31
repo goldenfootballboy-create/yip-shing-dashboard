e0595fd136c6eeb5c2910fef4a61b7c13fd4c617
goldenfootballboy-create <goldenfootballboy@gmail.com>
2025-10-31 14:50:27 +0800

    Update timeline_test.py

--- timeline_test.py
+++ timeline_test.py
@@ -292,11 +292,11 @@ if total_projects > 0:
                 k38 = 'KTA38' in desc
                 k50 = 'KTA50' in desc
 
-                c1, c2, c3 = st.columns([3, 0.2, 10])
+                c1, c2, c3 = st.columns([3, 0.2, 8])
                 with c1: st.write(row['Project_Name'])
                 with c2:
-                    if k38: st.image("https://i.imgur.com/koGZmUz.jpeg", width=35)
-                    elif k50: st.image("https://i.imgur.com/oJNLgDG.png", width=35)
+                    if k38: st.image("https://i.imgur.com/koGZmUz.jpeg", width=40)
+                    elif k50: st.image("https://i.imgur.com/oJNLgDG.png", width=40)
                 with c3:
                     st.markdown(f'<div class="custom-progress"><div class="custom-progress-fill" style="width:{progress}%;background:{color};"></div></div>', unsafe_allow_html=True)
                     pc1, pc2 = st.columns([1.5, 10])
