03606c25ef4577c4ccd2512167452c9ddb70bef3
goldenfootballboy-create <goldenfootballboy@gmail.com>
2025-10-31 14:45:13 +0800

    Update timeline_test.py

--- timeline_test.py
+++ timeline_test.py
@@ -317,7 +317,7 @@ if total_projects > 0:
                 b = 0
                 color = f'rgb({r},{g},{b})'
 
-                c1, c2, c3 = st.columns([9, 1, 30])
+                c1, c2, c3 = st.columns([5, 1, 30])
                 with c1:
                     st.write(f"**{item['name']}**")
                 with c2:
