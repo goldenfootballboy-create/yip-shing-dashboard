9e14cccf6ef64fcecf740adb5cb1bb8f382c4244
goldenfootballboy-create <goldenfootballboy@gmail.com>
2025-12-27 14:02:26 +0800

    Create supremacy_energy.py

---
+++ pages/supremacy_energy.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(page_title="SUPREMACY ENERGY", layout="wide")
+
+st.title("SUPREMACY ENERGY")
+
+st.markdown("""
+### 歡迎來到 SUPREMACY ENERGY 專區
+
+這裡可以放：
+- 公司介紹
+- 產品目錄
+- 聯絡資訊
+- 圖片展示
+- 其他專屬內容
+""")
+
+st.image("https://example.com/supremacy-logo.jpg", use_column_width=True)
+
+st.info("這是 app 內部的獨立頁面！")
